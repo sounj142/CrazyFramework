0d4d754e47abeac9d636050abf6dad50bc2eb0c6
Hoang Luong <hoang.luong@altsrc.net>
2020-06-06 21:20:04 +0700

    Add Angular client app placeholder

--- CrazyFramework.SpaApp.Angular/Startup.cs
+++ CrazyFramework.SpaApp.Angular/Startup.cs
@@ -1,6 +1,8 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.SpaServices.AngularCli;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
@@ -8,10 +10,24 @@ namespace CrazyFramework.SpaApp.Angular
 {
 	public class Startup
 	{
+		public Startup(IConfiguration configuration)
+		{
+			Configuration = configuration;
+		}
+
+		public IConfiguration Configuration { get; }
+
 		// This method gets called by the runtime. Use this method to add services to the container.
 		// For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
 		public void ConfigureServices(IServiceCollection services)
 		{
+			// In production, the Angular files will be served from this directory
+			services.AddSpaStaticFiles(configuration =>
+			{
+				configuration.RootPath = Configuration.GetValue<string>("ClientAppProductionRootPath");
+			});
+
+			//services.AddAuthorization();
 		}
 
 		// This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -22,14 +38,30 @@ namespace CrazyFramework.SpaApp.Angular
 				app.UseDeveloperExceptionPage();
 			}
 
+			app.UseHttpsRedirection();
+			app.UseStaticFiles();
+
+			if (!env.IsDevelopment())
+			{
+				app.UseSpaStaticFiles();
+			}
+
 			app.UseRouting();
 
-			app.UseEndpoints(endpoints =>
+			//app.UseAuthentication();
+			//app.UseAuthorization();
+
+			app.UseSpa(spa =>
 			{
-				endpoints.MapGet("/", async context =>
+				// To learn more about options for serving an Angular SPA from ASP.NET Core,
+				// see https://go.microsoft.com/fwlink/?linkid=864501
+
+				spa.Options.SourcePath = Configuration.GetValue<string>("ClientAppDevelopmentFolder");
+
+				if (env.IsDevelopment())
 				{
-					await context.Response.WriteAsync("Hello World!");
-				});
+					spa.UseAngularCliServer(npmScript: "start");
+				}
 			});
 		}
 	}
