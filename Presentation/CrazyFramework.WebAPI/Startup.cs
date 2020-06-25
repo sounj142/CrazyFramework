e6457aa8fe0efc1831bcd13cc1abda16ed5bd442
Hoang Luong <hoang.luong@altsrc.net>
2020-06-25 20:37:54 +0700

    Uncomment RequireAuthorization()

--- Presentation/CrazyFramework.WebAPI/Startup.cs
+++ Presentation/CrazyFramework.WebAPI/Startup.cs
@@ -60,7 +60,7 @@ namespace CrazyFramework.WebAPI
 
 			app.UseEndpoints(endpoints =>
 			{
-				endpoints.MapControllers(); //.RequireAuthorization();
+				endpoints.MapControllers().RequireAuthorization();
 			});
 		}
 	}
