1c3ba300e616af4d03475c3cbf1fdad08f76b44a
Hoang Luong <hoang.luong@altsrc.net>
2020-06-27 11:40:11 +0700

    remove unused using statements

--- Presentation/CrazyFramework.Client/Components/Layout/NavMenu.razor.cs
+++ Presentation/CrazyFramework.Client/Components/Layout/NavMenu.razor.cs
@@ -1,7 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using System.Threading.Tasks;
 using CrazyFramework.Client.Models.Users;
 using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Components.Authorization;
--- Presentation/CrazyFramework.Client/Pages/Index.razor.cs
+++ Presentation/CrazyFramework.Client/Pages/Index.razor.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-
-namespace CrazyFramework.Client.Pages
+﻿namespace CrazyFramework.Client.Pages
 {
 	public partial class Index
 	{
